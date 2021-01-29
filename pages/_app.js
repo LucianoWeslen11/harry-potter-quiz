1562d519c3caf3cf3a2c8a207442df151c716e20
Luciano Weslen <luciano.weslen11@gmail.com>
2021-01-29 12:04:49 -0300

    added head details

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import Head from 'next/head';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+import HeadConfig from './_head';
 
 import db from '../db.json';
 
@@ -33,15 +34,7 @@ const { theme } = db;
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Harry Potter Quiz</title>
-
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-
-        <meta property="og:image" content={db.bg} />
-        <meta name="theme-color" content="#cfb421" />
-      </Head>
+      <HeadConfig />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         {/* eslint-disable */}
