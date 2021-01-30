6e69cbce3ab7ea113583186ebfa265e06ea9a0fc
Luciano Weslen <luciano.weslen11@gmail.com>
2021-01-30 14:30:52 -0300

    added favicon

--- pages/_head.js
+++ pages/_head.js
@@ -26,6 +26,7 @@ export default function HeadConfig() {
 
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      <link rel="shortcut icon" href="/favicon.svg" />
     </Head>
   );
 }
